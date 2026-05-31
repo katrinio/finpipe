b136a85965d276da248eae995e20ed677b5f7db9
Katrin Torsunova <to.katrin.t@gmail.com>
2026-05-31 12:06:03 +0200

    upd test_integration_generate_invoice.py assert

--- tests/integration/invoice/test_integration_generate_invoice.py
+++ tests/integration/invoice/test_integration_generate_invoice.py
@@ -8,22 +8,24 @@ from src.services.invoice.invoice_models import InvoiceData
 def test_generate_invoice_creates_pdf(tmp_path):
     output_pdf = tmp_path / "test_invoice.pdf"
 
-    data = InvoiceData(
-        account_holder="John Doe",
-        account_holder_address="Amsterdam",
-        account_bic="ABNANL2A",
-        account_iban="NL91ABNA0417164300",
-        account_number="123456789",
-        amount="1000",
-        bank_name="ABN AMRO",
-        company_address="Belgrade",
-        company_name="Acme Ltd",
-        date_from="01.05.2020",
-        date_to="31.05.2020",
-        invoice_date="31.05.2020",
-        invoice_number="2026-05",
-        service_agreement_date="31.05.1994",
-    )
+    invoice_data = {
+        "account_holder": "John Doe",
+        "account_holder_address": "Amsterdam",
+        "account_bic": "ABNANL2A",
+        "account_iban": "NL91ABNA0417164300",
+        "account_number": "123456789",
+        "amount": "1000",
+        "bank_name": "ABN AMRO",
+        "company_address": "Belgrade",
+        "company_name": "Acme Ltd",
+        "date_from": "01.05.2020",
+        "date_to": "31.05.2020",
+        "invoice_date": "31.05.2020",
+        "invoice_number": "2026-05",
+        "service_agreement_date": "31.05.1994",
+    }
+
+    data = InvoiceData(**invoice_data)
 
     generate_invoice(
         template_path=TestData.TEMPLATE_PATH,
@@ -42,6 +44,5 @@ def test_generate_invoice_creates_pdf(tmp_path):
 
     text = "\n".join(page.extract_text() or "" for page in reader.pages)
 
-    assert "John Doe" in text
-    assert "1000" in text
-    assert "2026-05" in text
+    for key, value in invoice_data.items():
+        assert value in text, f"{key}: {value} не обнаружен в итоговом pdf."
