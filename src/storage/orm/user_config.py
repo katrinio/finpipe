4c0f95cba8d36f27f80f19f1ad6aa076d615b42d
Katrin Torsunova <to.katrin.t@gmail.com>
2026-06-07 17:20:16 +0200

    add user_config.py

---
+++ src/storage/orm/user_config.py
@@ -0,0 +1,32 @@
+"""ORM-сущность записи истории инвойсов."""
+
+from __future__ import annotations
+
+from datetime import datetime
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql.sqltypes import DateTime
+from traitlets import Integer
+
+from src.storage.orm.base import BaseStorage
+
+
+class UserConfig(BaseStorage):
+    """Запись о пользователях."""
+
+    __tablename__ = "user_config"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    telegram_id: Mapped[int] = mapped_column(Integer)
+    user_name: Mapped[str] = mapped_column(String)
+    account_holder = Mapped[str] = mapped_column(String)
+    account_holder_email: Mapped[str] = mapped_column(String)
+    account_holder_address: Mapped[str] = mapped_column(String)
+    bank_name = Mapped[str] = mapped_column(String)
+    account_number: Mapped[str] = mapped_column(String)
+    iban: Mapped[str] = mapped_column(String)
+    bic: Mapped[str] = mapped_column(String)
+    company_name: Mapped[str] = mapped_column(String)
+    company_address: Mapped[str] = mapped_column(String)
+    service_agreement_date: Mapped[datetime] = mapped_column(DateTime)
