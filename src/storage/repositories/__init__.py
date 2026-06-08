4f7ad5cad51db19fdfb9e1e0e2c092eb9b438da4
Katrin Torsunova <to.katrin.t@gmail.com>
2026-06-08 16:29:14 +0200

    drop rep

--- src/storage/repositories/__init__.py
+++ src/storage/repositories/__init__.py
@@ -1,8 +1,6 @@
-from src.storage.repositories.allowed_user_repository import AllowedUserRepository
 from src.storage.repositories.user_config_repository import SQLAlchemyUserConfigRepository, UserConfigRepository
 
 __all__ = [
-    "AllowedUserRepository",
     "SQLAlchemyUserConfigRepository",
     "UserConfigRepository",
 ]
