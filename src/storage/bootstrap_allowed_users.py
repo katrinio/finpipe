27baea54b212f55708f1229e382b5773781ea3a5
Katrin Torsunova <to.katrin.t@gmail.com>
2026-06-08 10:20:04 +0200

    secrets

--- src/storage/bootstrap_allowed_users.py
+++ src/storage/bootstrap_allowed_users.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 from src.constants import Dir
 from src.storage.database import Database, build_sqlite_url
 from src.storage.repositories.allowed_user_repository import SQLAlchemyAllowedUserRepository
+from src.utils.credentials import EnvVar
 
 
 def main() -> None:
@@ -12,7 +13,10 @@ def main() -> None:
     database.initialize_schema()
 
     repository = SQLAlchemyAllowedUserRepository(database.session)
-    repository.add(telegram_id=249517409, user_name="erichismyonlyfamily")
+    repository.add(
+        telegram_id=int(EnvVar.get_required_env("TELEGRAM_ADMIN_ID")),
+        user_name=EnvVar.get_required_env("TELEGRAM_ADMIN_USERNAME"),
+    )
 
 
 if __name__ == "__main__":
