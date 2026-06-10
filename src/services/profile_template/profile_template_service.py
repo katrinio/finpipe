da70c17b4baaa14da8450c535bbce1475e12c383
Katrin Torsunova <to.katrin.t@gmail.com>
2026-06-10 11:56:56 +0200

    base template parser

--- src/services/profile_template/profile_template_service.py
+++ src/services/profile_template/profile_template_service.py
@@ -2,6 +2,9 @@
 
 from __future__ import annotations
 
+import yaml
+
+from src.services.profile_template.profile_template import ProfileTemplate
 from src.services.profile_template.profile_template_validator import ProfileTemplateValidator
 
 
@@ -20,3 +23,17 @@ class ProfileTemplateService:
         ProfileTemplateValidator.validate_yaml(file_name)
         ProfileTemplateValidator.validate_size(file_size)
         ProfileTemplateValidator.validate_yaml_structure(file_bytes)
+
+    @classmethod
+    def parse_profile_template(cls, file_bytes: bytes) -> ProfileTemplate:
+        data = yaml.safe_load(file_bytes)
+        print(data)
+
+        return ProfileTemplate(**data)
+
+    @classmethod
+    def import_profile(cls, telegram_id: int, profile: ProfileTemplate) -> ProfileTemplate:
+        # CompanyProfile.upsert(...)
+        #
+        # BankDetails.upsert(...)
+        ...
