3875c92301dabe9072278500ff6f6dc88adca6d9
Katrin Torsunova <to.katrin.t@gmail.com>
2026-06-14 00:41:33 +0200

    fix tests

--- tests/unit/telegram/test_unit_telegram_gmail_connect.py
+++ tests/unit/telegram/test_unit_telegram_gmail_connect.py
@@ -54,13 +54,11 @@ def test_gmail_connect_sends_inline_authorization_button(
                         {
                             "text": GmailButtons.GMAIL_CONNECT_INLINE,
                             "url": authorization_url,
-                        }
-                    ],
-                    [
+                        },
                         {
                             "text": "🏠 Домой",
                             "callback_data": "🏠 Домой",
-                        }
+                        },
                     ],
                 ]
             },
