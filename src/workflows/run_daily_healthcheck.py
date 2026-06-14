dea89f3ad827cb7efc94709042db3f6d6e83a1c2
Katrin Torsunova <to.katrin.t@gmail.com>
2026-06-15 01:32:56 +0200

    debug

--- src/workflows/run_daily_healthcheck.py
+++ src/workflows/run_daily_healthcheck.py
@@ -7,7 +7,7 @@ from src.integrations.telegram.client import TelegramClient
 from src.logging_config import configure_logging
 from src.storage.dependencies import build_storage_dependencies
 from src.storage.orm import AllowedUser
-from src.utils.credentials import LOGGER
+from src.utils.credentials import LOGGER, EnvVar
 
 
 def main() -> int:
@@ -18,12 +18,11 @@ def main() -> int:
     try:
         build_storage_dependencies()
         bootstrap_primary_admin()
-        owner = AllowedUser.get_owner()
+        owner_telegram_id = EnvVar.get_required_env("BOT_OWNER_TELEGRAM_ID")
         allowed_users_count = len(AllowedUser.list_all())
-        if owner is None:
-            raise RuntimeError("Owner is not bootstrapped in storage")
+
         TelegramClient().send_daily_report(
-            owner.telegram_id,
+            int(owner_telegram_id),
             unit_status=os.environ["UNIT_STATUS"],
             integration_status=os.environ["INTEGRATION_STATUS"],
             telegram_status=os.environ["TELEGRAM_STATUS"],
