0e37fb178dca62cae09fe5fdc13166a9ea2e03f6
Katrin Torsunova <to.katrin.t@gmail.com>
2026-06-20 13:14:47 +0200

    move whoami from system menu to profile menu

--- src/integrations/telegram/ui/menu/system_menu.py
+++ src/integrations/telegram/ui/menu/system_menu.py
@@ -5,7 +5,6 @@ def build_system_menu(is_owner: bool = False) -> dict:
     keyboard = [
         [
             {"text": SystemButtons.EASY_START},
-            {"text": SystemButtons.WHOAMI},
         ],
         [
             {"text": NavigationButtons.HOME},
